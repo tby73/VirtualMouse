bae40db1a24f5166b3e5dcb9ba9d87c247a05c2e
setunknown <102586231+setunknown@users.noreply.github.com>
2023-10-19 09:33:59 +0200

    Update main.py

--- PR0_VirtualMouse/main.py
+++ PR0_VirtualMouse/main.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pyautogui as pg
+import screeninfo
 
 from hand_detector import HandDetector
 
@@ -13,7 +14,9 @@ SECONDARY_CAMERA = 1
 HANDS_LIMIT = 1
 
 # get screen size
-screen_width, screen_height = pg.size()
+screen_info = screeninfo.get_monitors()[0]  # Get the primary monitor information
+screen_width = screen_info.width
+screen_height = screen_info.height
 
 def main():
     video_cap = cv2.VideoCapture(DEFAULT_CAMERA)
@@ -26,6 +29,16 @@ def main():
         
         fingers_up = hand_detector.GetFingersUp(frame)
         click_length, [x1, y1, x2, y2, cx, cy] = hand_detector.ClickDistance(frame, 4, 8, 15, 3, draw=True)
+        
+        # detect if hand is in canvas and perform click if finger index 4 and 8 are close
+
+        if 0 < cx < screen_width and 0 < cy < screen_height:
+            corrected_cx = screen_width - cx  # Correcting the x-coordinate
+
+            pg.moveTo(corrected_cx, cy)
+
+            if click_length < 20:
+                pg.click()
 
         cv2.imshow("Virtual Mouse v1.0", frame)
 
